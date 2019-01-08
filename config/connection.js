c0b94af1599efb26033c962b1eb1d6bc3f74c636
Darren Anastasoff <soleman16@gmail.com>
2019-01-07 21:33:38 -0500

    updated connection.js with remote database info

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,22 @@
 
 // Require mysql
 require("dotenv").config();
-var mysql = require("mysql");
+const mysql = require("mysql");
+let connection;
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  port: process.env.PORT,
-  user: process.env.MYSQL_USER_NAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.DATABASE
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.MYSQL_USER_NAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DATABASE
+  });
+}
 
 // Connect to the database
 connection.connect(function(err) {

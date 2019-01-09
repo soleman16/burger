8dc1fccbf93ae2994727e4bd419a596c33ab9282
Darren Anastasoff <soleman16@gmail.com>
2019-01-08 22:08:46 -0500

    added burger restaurant theme

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -43,4 +43,20 @@ $(function() {
       );
     });
   });
+
+  function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+    document.getElementById("demo").innerHTML = "Started to drag the p element";
+  }
+  
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("demo").innerHTML = "The p element was dropped";
+  }
   
